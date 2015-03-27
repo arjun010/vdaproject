969e362af776cfbca78d78edfbdcff559d4b9599
John Thompson <jrthompson@gatech.edu>
2015-03-27 12:27:20 -0400

    Accordion function in document view. Also added in the view tally for each document.

--- client/js/document-tool.js
+++ client/js/document-tool.js
@@ -10,13 +10,30 @@
         // Create all of the templates
         var tmpls = $('#docTemplate').tmpl(data.documents);
 
-        console.log(tmpls);
         $(tmpls[0]).appendTo('#document-container');
+        $(tmpls[1]).appendTo('#document-container');
+        $(tmpls[2]).appendTo('#document-container');
+        $(tmpls[3]).appendTo('#document-container');
     };
 
     docTool.add = function(doc, docId){
         // If the document object wasn't passed in then find it based on id
         doc = doc || data.documents.filter(function(d){return docId == d.id;});
+        // Create the template for that data item and add it to the document container
+        // TODO should new documents be put at the top or bottom?
+        var tmpl = $('#docTemplate').tmpl([doc]).appendTo('#document-container');
+    };
+
+    docTool.collapse = function(d){
+        var doc = $(d).parent();
+        var text = doc.find(".doc-item-text");
+        // If the text was hidden then we are know opening it, so add to view count
+        if(text.css('display') == 'none'){
+            doc.tmplItem().data.views++;
+            $(d).find(".views").html(doc.tmplItem().data.views);
+            console.log($(d).find(".views"));
+        }
+        text.slideToggle();
     };
 
     docTool.collapseAll = function(){
